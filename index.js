df173fd71ce83d088d0d128ad6c288e711317d55
Your Name <you@example.com>
2023-02-26 18:08:45 +0700

    belajar javascript

---
+++ index.js
@@ -0,0 +1,34 @@
+//arraay
+
+/*const name = []
+name.push("riki")
+name.push("kharisma")
+name.push("putra")
+
+name[0] = "progaming"
+name[1] = "developer"
+delete [3]
+
+console.table(name)*/
+
+//object
+
+/*const person = {};
+
+person["Nama"] = "Riki"
+person["Kelas"] = "11"
+
+delete person["Kelas"]
+console.table(person)*/
+
+
+// mengambil pernama
+const person = {
+    nama:"Riki",
+    kelas:"11",
+    umur:"17 otw 18"
+}
+
+/*console.table(`Nama : ${person.nama}`)
+console.table(`Nama : ${person.kelas}`)
+console.table(`Nama : ${person.umur}`)*/
