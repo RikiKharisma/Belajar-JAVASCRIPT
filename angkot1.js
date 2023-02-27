cbede5b4e8fe33f84f3d696dabc392222b431ed0
Your Name <you@example.com>
2023-02-28 06:44:40 +0700

    for dan while

---
+++ angkot1.js
@@ -0,0 +1,12 @@
+var jmlangkot=10;
+var angkotBeroperasi=6
+var noAngkot=1;
+
+while(noAngkot <=angkotBeroperasi){
+    console.log("Angkot No :" + angkotBeroperasi + "Beroperasi dengan baik")
+    noAngkot++
+}
+
+for(noAngkot = 7; noAngkot<=jmlangkot; noAngkot++ ){
+    console.log("Angkot No : " +noAngkot + "Lembur")
+}
