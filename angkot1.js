096131231c32469a9db732ea9032555cb0612cf6
Your Name <you@example.com>
2023-03-01 18:08:23 +0700

    ifEls

--- angkot1.js
+++ angkot1.js
@@ -3,10 +3,10 @@ var angkotBeroperasi=6
 var noAngkot=1;
 
 while(noAngkot <=angkotBeroperasi){
-    console.log("Angkot No :" + angkotBeroperasi + "Beroperasi dengan baik")
+    console.log("Angkot No :" + angkotBeroperasi + "Bekerja")
     noAngkot++
 }
 
-for(noAngkot = 7; noAngkot<=jmlangkot; noAngkot++ ){
-    console.log("Angkot No : " +noAngkot + "Lembur")
+for(noAngkot = 7; noAngkot<=jmlangkot; noAngkot++){
+    console.log("Angkot No : " + noAngkot + "Lembur")
 }
---
+++ ifels.js
@@ -0,0 +1,11 @@
+var jmlangkot=10;
+var angkotBeroperasi=6
+var noAngkot=1;
+
+for(noAngkot; noAngkot<=jmlangkot; noAngkot++){
+    if(noAngkot <=angkotBeroperasi){
+        console.log("Angkot No : " +noAngkot + "Beroperasi")
+    }else{
+        console.log("Angkot No : " +noAngkot + "Lembur")
+    }
+}
