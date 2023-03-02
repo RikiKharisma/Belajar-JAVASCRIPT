8e9b2b4fb2e0301b50d5f7af60a9e67f4068a403
Your Name <you@example.com>
2023-03-02 13:37:54 +0700

    v1

---
+++ v1/data.js
@@ -0,0 +1,13 @@
+var jumlahAngkot = 10;
+var angkotBeroperasi=6;
+var noAngkot=1;
+
+for(noAngkot; noAngkot<jumlahAngkot; noAngkot++){
+    if(noAngkot <=angkotBeroperasi && noAngkot !==2){
+        console.log("Angkot No :" + noAngkot + "Beroperasi dengan baik")
+    }else if(noAngkot ===8 || noAngkot ===2){
+        console.log("Angkot No : "+ noAngkot + "Rusak")
+    }else{
+        console.log("Angkot No :" + noAngkot + "Lembur")
+    }
+}
---
+++ v1/index.js
@@ -0,0 +1,11 @@
+const data = "air"
+
+switch(data){
+    case "ikan":
+        console.log("Ikan")
+        break;
+    case "kuda":
+        console.log("Kuda")
+        default:
+            console.log("Error")
+}
