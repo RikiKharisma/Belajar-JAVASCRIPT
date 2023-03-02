eaae4dcd75e8fe146513f24df4c61077a0005121
Your Name <you@example.com>
2023-03-02 19:15:35 +0700

    do while

--- v1/data.js
+++ v1/data.js
@@ -3,9 +3,9 @@ var angkotBeroperasi=6;
 var noAngkot=1;
 
 for(noAngkot; noAngkot<jumlahAngkot; noAngkot++){
-    if(noAngkot <=angkotBeroperasi && noAngkot !==2){
+    if(noAngkot <=angkotBeroperasi && noAngkot !==2 && noAngkot !==3){
         console.log("Angkot No :" + noAngkot + "Beroperasi dengan baik")
-    }else if(noAngkot ===8 || noAngkot ===2){
+    }else if(noAngkot ===8 || noAngkot ===2 || noAngkot ===3){
         console.log("Angkot No : "+ noAngkot + "Rusak")
     }else{
         console.log("Angkot No :" + noAngkot + "Lembur")
---
+++ v1/dowhile.js
@@ -0,0 +1,6 @@
+let data = 1
+
+do{
+    console.log(`Angka ke ${data}`)
+    data++
+}while(data <=10)
