331c8ca47f44f019a5a9b9931c9513c7104b27a7
Your Name <you@example.com>
2023-03-02 20:39:34 +0700

    arrow function

--- v1/function.js
+++ v1/function.js
@@ -1,7 +1,43 @@
 // function parameter
 
-function bioData (fristName,LastName){
+/*function bioData (fristName,LastName){
     console.log(`${fristName} ${LastName}`)
 }
 
-bioData("Riki","Kharisma")
+bioData("Riki","Kharisma")*/
+
+
+// return vlue
+/*function sayHello (fristName,LastName){
+    const say= `Hello ${fristName} ${LastName}`
+    return say;
+}
+
+const result = sayHello("Riki","Kharisma")
+console.log(`${result}`)*/
+
+/*function getFinalVlue (value){
+    if(value >90){
+        return "A"
+    }else if(value >80){
+        return "B"
+    }else if(value >70){
+        return "C"
+    }else{
+        return "D"
+    }
+}
+
+const final = getFinalVlue(95)
+console.log(final)*/
+
+
+
+// arrow function
+
+const nama = (name)=>{
+    const say = `Hello ${name}`
+    console.log(say)
+}
+nama("riki")
+
