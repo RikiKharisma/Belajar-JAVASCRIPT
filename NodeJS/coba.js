df6d9accd40b355d04af731762582f5640198868
Your Name <you@example.com>
2023-03-03 16:59:56 +0700

    NodeJS Module System

---
+++ NodeJS/coba.js
@@ -0,0 +1,9 @@
+/*function data (Nama){
+    return `Hello Nama saya ${Nama}`
+}
+
+const nama = 12
+
+module.exports.data = data
+module.exports.nama = nama*/
+
---
+++ NodeJS/index.js
@@ -0,0 +1,5 @@
+
+// NodeJS Module System
+/*const coba = require("./coba")
+
+console.log(coba.data("Riki"),coba.nama)*/
