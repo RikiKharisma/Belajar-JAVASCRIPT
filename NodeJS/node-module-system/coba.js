29e1b5286c5945d07dfea361923c41273e3f23d9
Your Name <you@example.com>
2023-03-04 23:53:03 +0700

    node-module-system

---
+++ NodeJS/node-module-system/coba.js
@@ -0,0 +1,34 @@
+// function data (Nama){
+//     return `Hello Nama saya ${Nama}`
+//  }
+
+//  const nama = 12
+
+// const names = "subroto"
+
+// const mahasiswa = {
+//     nama :"subroto",
+//     kelas:"11 tkjc",
+
+//     cetakMhs(){
+//         return  `Hallo Nama Saya ${nama} Saya kelas ${this.kelas}`
+//     }
+// }
+
+// // module.exports.data = data
+// // module.exports.nama = nama
+// // module.exports.names=names
+// // module.exports.mahasiswa=mahasiswa
+
+// module.exports = {
+//     data: data,
+//     nama: nama,
+//     mahasiswa : mahasiswa
+
+
+// }
+
+
+
+// NodeJS Core Module
+
---
+++ NodeJS/node-module-system/index.js
@@ -0,0 +1,9 @@
+
+// // NodeJS Module System
+
+// const coba = require("./coba") // localmodul
+
+// console.log(coba.data("subroto"), coba.nama, coba.names, coba.mahasiswa.cetakMhs())
+
+
+
